d912f295cd3e1a1b149645a5d75601b5df3d23ad
Коваленко Владислав Анатольевич <vladmallory0321@gmail.com>
2025-12-20 17:24:10 +1000

    - Начинаем

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"ProxyMaster_v2/internal/app"
+	"log"
+)
 
+func main() {
+	// 1. сборка приложения
+	myApp, err := app.New()
+	if err != nil {
+		log.Fatal("ошибка сборки приложения: %w", err)
+	}
+
+	// 1. запуск приложения
+	myApp.Run()
 }
---
+++ internal/app/app.go
@@ -0,0 +1,14 @@
+package app
+
+type App struct {
+}
+
+func New() (*App, error) {
+
+	return &App{}, nil
+
+}
+
+func (a *App) Run() {
+
+}
