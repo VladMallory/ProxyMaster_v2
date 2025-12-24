d0f641ff2bc74fda540747150e8e237693fe2620
qury53 <lamerxdxd@gmail.com>
2025-12-24 15:56:39 +0300

    refactored

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,8 +1,17 @@
 package domain
 
-import "context"
+import (
+	"ProxyMaster_v2/internal/models"
+	"context"
+)
 
 // RemnawaveClient - то как мы хотим получать информацию
 type RemnawaveClient interface {
+	Login(ctx context.Context, username string, password string) error
 	GetServiceInfo(ctx context.Context, serviceID string) (string, error)
+	GetUUIDByUsername(username string) (string, error)
+	CreateClient(userData *models.CreateRequestUserDTO) error
+	ExtendClientSubscription(userUUID string, days int) error
+	EnableClient(userUUID string) error
+	DisableClient(userUUID string) error
 }
