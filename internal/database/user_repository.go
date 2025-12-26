389e9037f65e8f7569ee0d19f6b7d43cca214683
qury53 <lamerxdxd@gmail.com>
2025-12-26 09:59:21 +0300

    started database_package

---
+++ internal/database/user_repository.go
@@ -0,0 +1,13 @@
+package database
+
+import "github.com/jmoiron/sqlx"
+
+type UserStorage struct {
+	db *sqlx.DB
+}
+
+func NewUserStorage(db *sqlx.DB) *UserStorage {
+	return &UserStorage{
+		db: db,
+	}
+}
