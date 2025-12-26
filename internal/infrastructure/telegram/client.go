f7ccee4726ea36dfb377c7c88f06a1c1eb1ddda5
Владислав Коваленко Анатольевич <vladmallory0321@gmail.com>
2025-12-27 01:28:39 +1000

    Рефакторинг комментариев

--- internal/infrastructure/telegram/client.go
+++ internal/infrastructure/telegram/client.go
@@ -7,46 +7,47 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-// Command - интферйс для всех команд бота, /start /help и прочих
-// нужен чтобы следовать принципам SOLID. Закрыт для изменений
+// Command - интерфейс для всех команд бота, /start /help и прочих
+// нужен, чтобы следовать принципам SOLID. Закрыт для изменений
 // добавлять будем через мапу, так минимальные шансы что-то
 // сломать из старого кода
-type Command interface {
-	// то какая строка. /start, /help и т.д.
+type command interface {
+	// Name то какая строка. /start, /help и т.д.
 	Name() string
 
-	// что делаем с строкой
-	// 1. tgbotapi.Update - внутри Update лежит все что прислал пользователь
+	// Execute что делаем со строкой
+	// 1. tgbotapi. Update - внутри Update лежит все что прислал пользователь
 	// текст сообщения ("Привет", "/start"), кто он (ChatID, UserID), имя и т.д.
-	// 2. tgbitapi.BotAPI - делает запросы в телеграм. Send, DeleteMessage, KickChatMember (выгнать) и т.д.
+	// 2. tgbotapi. BotAPI - делает запросы в телеграм. Send, DeleteMessage, KickChatMember (выгнать) и т.д.
 	Execute(update tgbotapi.Update, bot *tgbotapi.BotAPI) error
 }
 
 // Client - зависимости для телеграм
 type Client struct {
-	// само апи телеграмма
+	// Само апи телеграмма
 	bot *tgbotapi.BotAPI
-	// команды которые бот должен обработать. /start /help и т.д.
-	commands map[string]Command
+	// Команды которые бот должен обработать. /start /help и т.д.
+	commands map[string]command
 }
 
 // NewClient - экземпляр бота
 func NewClient(bot *tgbotapi.BotAPI) *Client {
+	fmt.Println("Создан экземпляр TelegramClient")
 	return &Client{
 		bot:      bot,
-		commands: make(map[string]Command),
+		commands: make(map[string]command),
 	}
 }
 
 // RegisterCommand - занимается регистрацией команд в боте
-func (c *Client) RegisterCommand(cmd Command) {
+func (c *Client) RegisterCommand(cmd command) {
 	c.commands[cmd.Name()] = cmd
 }
 
-// Run - запуск запуск цикла получения сообщения
-func (h *Client) Run() {
+// Run - запуск цикла получения сообщения
+func (c *Client) Run() {
 	// получаем канал обновлений
-	updates, err := h.initUpdatesChannel()
+	updates, err := c.initUpdatesChannel()
 	if err != nil {
 		slog.Error("ошибка при запуске прослушивания", "error", err)
 		return
@@ -62,11 +63,11 @@ func (h *Client) Run() {
 		fmt.Println("telegram:", update.Message.From.ID, update.Message.Text)
 
 		// сообщение идет в роутинг команд
-		h.handleUpdate(update)
+		c.handleUpdate(update)
 	}
 }
 
-// HandleCommands - роутинг команды. Принимает сообщение которое
+// HandleCommands роутинг команды. Принимает сообщение которое
 // пришло в Run() и решает что дальше с ним делать
 func (c *Client) handleUpdate(update tgbotapi.Update) {
 	cmdName := update.Message.Command()
@@ -89,11 +90,11 @@ func (c *Client) handleUpdate(update tgbotapi.Update) {
 // Это лишние обращения к процессору и серверам телеги
 // ---
 // 2. подход Long Polling: подходишь к почтовому ящику, открываешь
-// его. стоишь и ждешь так 60 секунд если письмо есть,
+// его. Стоишь и ждешь так 60 секунд если письмо есть,
 // берем. Если не нет, то закрываем ящик, а
 // потом опять открываем и ждем 60 секунд
 func (c *Client) initUpdatesChannel() (tgbotapi.UpdatesChannel, error) {
-	// это настройка запроса. 0 - дает все с самого
+	// Это настройка запроса. 0 - дает все с самого
 	// начала и то что еще не обработал
 	u := tgbotapi.NewUpdate(0)
 
@@ -105,7 +106,6 @@ func (c *Client) initUpdatesChannel() (tgbotapi.UpdatesChannel, error) {
 
 // --------- команды ---------
 
-// StartCommand
 type StartCommand struct{}
 
 func (s *StartCommand) Name() string {
