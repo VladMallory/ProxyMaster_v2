575d63acadc00253edea608edc98f2510ba36e34
Владислав Коваленко Анатольевич <vladmallory0321@gmail.com>
2025-12-30 12:40:38 +1000

    Мелкие правки

--- internal/service/subscription.go
+++ internal/service/subscription.go
@@ -9,20 +9,21 @@ import (
 	"strconv"
 )
 
-// SubscriptionService реализация бизнес логики подписок
+// SubscriptionService представляет собой сервис для управления подписками клиентов с помощью remnawave.
 type SubscriptionService struct {
 	remna domain.RemnawaveClient
 }
 
-// NewSubscriptionService конструктор сервиса
+// NewSubscriptionService конструктор сервиса.
 func NewSubscriptionService(remna domain.RemnawaveClient) *SubscriptionService {
 	return &SubscriptionService{
 		remna: remna,
 	}
 }
 
+// ActivateSubscription активирует подписку клиенту telegram на указанное количество месяцев.
 func (s *SubscriptionService) ActivateSubscription(telegramID int64, months int) (string, error) {
-	days := months * 30
+	totalDays := months * 30
 	username := strconv.FormatInt(telegramID, 10)
 
 	// Проверяем есть ли пользователь в панели
@@ -30,20 +31,20 @@ func (s *SubscriptionService) ActivateSubscription(telegramID int64, months int)
 	if err != nil {
 		if errors.Is(err, remnawave.ErrNotFound) {
 			log.Println("СЕРВИС: пользователь не найден, создаем нового:", username)
-			err = s.remna.CreateUser(username, days)
+			err = s.remna.CreateUser(username, totalDays)
 			if err != nil {
 				return "", fmt.Errorf("ошибка создания пользователя: %w", err)
 			}
-			return fmt.Sprintf("пользователь %s создан на %d дней", username, days), nil
+			return fmt.Sprintf("пользователь %s создан на %d дней", username, totalDays), nil
 		}
 		return "", fmt.Errorf("ошибка поиска пользователя: %w", err)
 	}
 
 	log.Println("СЕРВИС: пользователь найден")
 
-	err = s.remna.ExtendClientSubscription(userUUID, username, days)
+	err = s.remna.ExtendClientSubscription(userUUID, username, totalDays)
 	if err != nil {
 		return "", fmt.Errorf("ошибка продления подписки пользователю: %v %v", username, err)
 	}
-	return fmt.Sprintf("подписка для пользователя %s продлена на %d дней", username, days), nil
+	return fmt.Sprintf("подписка для пользователя %s продлена на %d дней", username, totalDays), nil
 }
