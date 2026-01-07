ad38a281ac541fc6942d5cf42a9ae322ec601537
qury53 <lamerxdxd@gmail.com>
2026-01-07 20:37:04 +0300

    сделал rest api

---
+++ internal/delivery/restAPI/handlers.go
@@ -0,0 +1,83 @@
+package restapi
+
+import (
+	"ProxyMaster_v2/internal/infrastructure/remnawave"
+	"ProxyMaster_v2/internal/models"
+	"ProxyMaster_v2/internal/service"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Hadnler структура отвечает за хэндлеры
+type Handler struct {
+	remnawaveClient remnawave.RemnaClient
+}
+
+// NewHandler конструктор Handler
+func NewHandler(remnawaveClient remnawave.RemnaClient) *Handler {
+	return &Handler{
+		remnawaveClient: remnawaveClient,
+	}
+}
+
+// respondWithJSON - вспомагательная функция для отправки JSON ответа
+func (H *Handler) responseWithJSON(w http.ResponseWriter, payload any, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(statusCode)
+
+	json.NewEncoder(w).Encode(payload)
+}
+
+// respondError - тоже самое что и respondWithJSON, но название другое для удобства
+// ну и почему бы нет
+func (h *Handler) respondError(w http.ResponseWriter, payload any, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(statusCode)
+
+	json.NewEncoder(w).Encode(payload)
+}
+
+// newPayload конструктор тела ответа
+func (h *Handler) newPayload(statusCode int, message string) *models.ErrResponseRestAPI {
+	return &models.ErrResponseRestAPI{
+		StatusCode: statusCode,
+		ErrMessage: message,
+	}
+}
+
+// GetSubscriptionURL хэндлер роута /url/{username}, возвращает SubscriptionURL
+// по имени пользователя
+func (h *Handler) GetSubscriptionURL(w http.ResponseWriter, r *http.Request) {
+	username := mux.Vars(r)["username"]
+
+	if username == "" {
+		h.respondError(w, "username cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	url, err := service.GetURLSubscription(&h.remnawaveClient, username)
+
+	switch err {
+	case remnawave.ErrBadRequestUsername:
+		h.respondError(w, h.newPayload(http.StatusBadRequest, "bad request"), http.StatusBadRequest)
+		return
+	case remnawave.ErrInternalServerError:
+		h.respondError(w, h.newPayload(http.StatusInternalServerError, "internal server error"), http.StatusInternalServerError)
+		return
+	case remnawave.ErrNotFound:
+		h.respondError(w, h.newPayload(http.StatusNotFound, "user not found"), http.StatusNotFound)
+		return
+	}
+
+	if url == "" {
+		h.respondError(w, "failed to get url", http.StatusInternalServerError)
+		return
+	}
+
+	payload := models.GetSubscriptionURLResponse{URL: url}
+	h.responseWithJSON(w, payload, http.StatusOK)
+}
