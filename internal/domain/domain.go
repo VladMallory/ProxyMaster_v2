c8c4e58428f7df60287f683d295b8b79b71668c8
Коваленко Владислав Анатольевич <vladmallory0321@gmail.com>
2026-01-14 18:30:12 +1000

    telegram.go

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -31,7 +31,6 @@ const (
 	ViewTypeConnect            ViewType = "connect"
 	ViewTypeSubscriptionResult ViewType = "subscription_result"
 	ViewTypeMain               ViewType = "main"
-	ViewTypeError              ViewType = "unknown view type"
 )
 
 // Ошибка приложения
